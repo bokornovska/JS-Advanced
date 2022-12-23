3735dbbdd835de4af2406386863c40a2099bc990
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2022-12-23 09:06:43 +0200

    Advanced Functions - lab

---
+++ 06.Advanced Functions/06.01.Advanced Functions-lab/01-areaAndVolumeCalculator.js
@@ -0,0 +1,36 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+function solve(area, vol, input) {
+
+    let coordinates = JSON.parse(input); // string => array of objects
+    // console.log(coordinates)
+
+    let result = [];
+    for (let points of coordinates) {
+        result.push({
+            area: area.call(points),
+            volume: vol.call(points)
+        })
+    }
+
+    return result
+}
+
+function fancySolve(area, vol, input) {
+    return JSON.parse(input)
+        .map(x => ({
+                area: area.call(x),
+                volume: vol.call(x),
+            })
+        );
+}
+
+solve(area, vol, `[
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}
+    ]`)
